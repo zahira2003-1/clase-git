91454f0e7372903b22ed4cf6b193e17ddaa02430
Zahira <zahiramoises@hotmail.com.ar>
2025-10-16 22:23:30 -0300

    Hola es mi primera vez

---
+++ ejemplo.py
@@ -0,0 +1,35 @@
+valor_moneda = 1515
+pesos = float(input("ingrese cantiddad de pesos: "))
+#print(type(pesos))
+total = pesos/valor_moneda
+print("Usted tiene U$D: ",round(total,2))
+
+
+
+
+PI = 3.14
+radio = float(input("ingrese el valor del radio (en metros): "))
+area=PI * radio ** 2
+print(f"El valor del area es: {area}")
+
+
+nombre2 = input("ingrese su nombre: ")
+print("hola " + nombre2*5)
+
+
+
+
+
+nombre3 = input("Ingrese su nombre: ")
+print(f"hola {nombre3.upper()}")
+
+
+numero=float(input ("Ingrese un numero: "))
+total=numero+5
+print("El resultado es: ")
+
+
+
+nombre4 = input("Ingrese su nombre: ")
+input ("Ingrese su apellido: ")
+print("Bienvendio {nombre4+apellido ()} ")
